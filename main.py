3657aa65bc896f27615f96f61a681c41f2974976
James <j1459.j1459@gmail.com>
2015-10-22 22:22:38 +0800

    tidy up

--- main.py
+++ main.py
@@ -11,53 +11,12 @@
 import logging
 import re
 import os
-import sqlalchemy# Database library
 
 import imgur
 import config # Settings and configuration
 import lockfiles # MutEx lockfiles
 from utils import * # General utility functions
-import sql_functions# Database interaction
-from tables import Board# Table definitions
 
-
-def save_vocaroo_link(vocaroo_link,output_dir):
-    """Take a vocaroo link and save it
-    example input:
-        http://vocaroo.com/i/s0xtktsit8rE
-    """
-    logging.debug("vocaroo_link: "+repr(vocaroo_link))
-    #logging.debug("output_dir: "+repr(output_dir))
-
-    # Grab the ID of the vocaroo
-    vocaroo_id_search = re.search("""vocaroo.com/i/(\w+)""", vocaroo_link, re.IGNORECASE)
-    vocaroo_id = vocaroo_id_search.group(1)
-    logging.debug("vocaroo_id: "+repr(vocaroo_id))
-
-    # Generate output path
-    output_filename = vocaroo_id+".mp3"
-    output_path = os.path.join(output_dir, output_filename)
-    logging.debug("output_path: "+repr(output_path))
-
-    # Check if file already exists
-    if os.path.exists(output_path):
-        logging.debug("File exists, skipping download.")
-        return
-
-    # Generate download link
-    # http://vocaroo.com/media_command.php?media=s0xtktsit8rE&command=download_mp3
-    #download_url = "http://vocaroo.com/media_command.php?media="+vocaroo_id+"&command=download_mp3"
-    logging.debug("download_url: "+repr(download_url))
-
-    # Try saving download link
-    vocaroo_file = get_url(download_url)
-    save_file(
-    	file_path=output_path,
-    	data=vocaroo_file,
-    	force_save=True,
-    	allow_fail=False)
-    logging.info("Saved file.")
-    return
 
 
 def find_imgur_links_in_db(session,start_id,stop_id,output_dir):
